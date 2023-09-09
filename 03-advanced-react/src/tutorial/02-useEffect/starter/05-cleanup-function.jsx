5562696258b9e4c26daae5a246b4d46fb0237d87
Sagar Shrivastava <sagar.shrivastava889@gmail.com>
2023-09-09 17:03:00 +0530

    completed till part 3 of section 3 and completed fundamental project tours

--- 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+  return (
+    <div>
+      <button className="btn" onClick={() => setToggle(!toggle)}>
+        Toggle
+      </button>
+      {toggle && <RandomComponent />}
+    </div>
+  );
+};
+
+const RandomComponent = () => {
+  useEffect(() => {
+    console.log("component mounted");
+  }, []);
+  return (
+    <div className="alert alert-success">
+      <h1>Hello World</h1>
+    </div>
+  );
 };
 
 export default CleanupFunction;
--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,15 @@
+import { useState } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [toggle, setToggle] = useState(false);
+  return (
+    <div>
+      <button className="btn" onClick={() => setToggle(!toggle)}>
+        toggle
+      </button>
+      {toggle && <div className="alert alert-danger">Hello world</div>}
+    </div>
+  );
 };
 
 export default ToggleChallenge;
