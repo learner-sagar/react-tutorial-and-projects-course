19eca1b8d5565f216b999c778f4bbfdfbad4200c
Sagar Shrivastava <sagar.shrivastava889@gmail.com>
2023-09-13 00:34:55 +0530

    completed strpi submenus project

--- 04-fundamental-projects/13-strapi-submenus/starter/src/App.jsx
+++ 04-fundamental-projects/13-strapi-submenus/starter/src/App.jsx
@@ -1,4 +1,18 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
+import { useGlobalContext } from "./context";
+
 const App = () => {
-  return <h2>Strapi Starter</h2>;
+  const { isSidebarOpen } = useGlobalContext();
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Sidebar />
+      <Submenu />
+    </main>
+  );
 };
 export default App;
